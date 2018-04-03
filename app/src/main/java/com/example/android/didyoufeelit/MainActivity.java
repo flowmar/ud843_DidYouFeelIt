60d0a8415975703a10b83b351ef78437f72668de
Omar Imam <flowmar@kineticvortex.com>
2018-04-03 13:12:49 -0500

    reformatted code to properly fetch data via the Internet

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,9 +15,12 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import java.net.URL;
 
 /**
  * Displays the perceived strength of a single earthquake event based on responses from people who
@@ -25,33 +28,53 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
-    /** URL for earthquake data from the USGS dataset */
-    private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
+  /** URL for earthquake data from the USGS dataset */
+  private static final String USGS_REQUEST_URL =
+      "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    FetchURLsTask fetchURLsTask = new FetchURLsTask();
+    fetchURLsTask.execute(USGS_REQUEST_URL);
+  }
+
+  /** Update the UI with the given earthquake information. */
+  private void updateUi(Event earthquake) {
+    TextView titleTextView = (TextView) findViewById(R.id.title);
+    titleTextView.setText(earthquake.title);
+
+    TextView tsunamiTextView = (TextView) findViewById(R.id.number_of_people);
+    tsunamiTextView.setText(getString(R.string.num_people_felt_it, earthquake.numOfPeople));
+
+    TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
+    magnitudeTextView.setText(earthquake.perceivedStrength);
+  }
+
+  /** Create a private class that extends the AsyncTask class, */
+  private class FetchURLsTask extends AsyncTask<String, Void, Event> {
+    // This is called on a background thread
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+    protected Event doInBackground(String... urls) {
+      // Don't perform the request if there are no URLs, or if the first URL is null
+      if (urls.length < 1 || urls[0] == null) {
+        return null;
+      }
+      // Perform the HTTP request for earthquake data and process the response.
+      Event result = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
+      return result;
     }
-
-    /**
-     * Update the UI with the given earthquake information.
-     */
-    private void updateUi(Event earthquake) {
-        TextView titleTextView = (TextView) findViewById(R.id.title);
-        titleTextView.setText(earthquake.title);
-
-        TextView tsunamiTextView = (TextView) findViewById(R.id.number_of_people);
-        tsunamiTextView.setText(getString(R.string.num_people_felt_it, earthquake.numOfPeople));
-
-        TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
-        magnitudeTextView.setText(earthquake.perceivedStrength);
+    // This is called on the main thread
+    @Override
+    protected void onPostExecute(Event result) {
+      // If there is no result, do nothing
+      if (result == null) {
+        return;
+      }
+      // Update the information displayed to the user.
+      updateUi(result);
     }
+  }
 }
